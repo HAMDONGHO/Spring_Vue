15f0f429a99619eb5dea080cebd473e2c219afff
HAMDONGHO <ii8858@naver.com>
2021-01-29 14:29:07 +0900

    1/29 Web Complete

--- src/main/java/todo/controller/BoardController.java
+++ src/main/java/todo/controller/BoardController.java
@@ -1,11 +1,19 @@
 package todo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//import com.sun.org.apache.bcel.internal.generic.NEW;
 
 import todo.service.BoardService;
+import todo.domain.BoardDTO;
 
 //해당 클래스가 유저 요청과 응답을 처리하는 UI 컨트롤러 클래스임을 의미
 @Controller
@@ -17,17 +25,87 @@ public class BoardController {
 	//URI매핑!! value = ""
 	@GetMapping(value = "/board/write.do")
 	//Model인터페이스는 데이터를 뷰로 전달함
-	public String openBoardWrite(Model model) {
-		//return은 html 재외하도됨
+	//뷰(화면)에서 전달받은 파라미터를 처리하는데 사용, 
+	public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
 		
-		String title = "제목";
-		String content = "내용";
-		String writer= "홍길동";
+		if (idx == null) {
+			model.addAttribute("board", new BoardDTO());
+		}
+		else {
+			BoardDTO board = boardService.getBoardDetail(idx);
+			if (board == null) {
+				return "redirect:/board/list.do";
+			}
+			model.addAttribute("board",board);
+		}
 		
 		//addAttribute메서드는 html로 데이터를 전달 가능(html은 ${}으로 데이터에 접근
-		model.addAttribute("t", title);
-		model.addAttribute("c",content);
-		model.addAttribute("w", writer);
+		//return은 html 재외하도됨
 		return "board/write";
+	}
+	
+	//웹에서 URL매핑한 것을 어노테이션으로 묶어서 알려줌. 후에 register 서비스로 선언
+	@PostMapping(value = "/board/register.do")
+	public String registerBoard(final BoardDTO params) {
+		try {
+			boolean isRegistered = boardService.registerBoard(params);
+			if (isRegistered == false) {
+				// TODO => 게시글 등록에 실패하였다는 메시지를 전달
+			}
+		} catch (DataAccessException e) {
+			// TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+
+		} catch (Exception e) {
+			// TODO => 시스템에 문제가 발생하였다는 메시지를 전달
+		}
+
+		return "redirect:/board/list.do";
+	}
+	
+	@GetMapping(value = "/board/list.do")
+	public String openBoardList(Model model) {
+		List<BoardDTO> boardList = boardService.getBoardList();
+		model.addAttribute("boardList", boardList);
+
+		return "board/list";
+	}
+	
+	@GetMapping(value = "/board/view.do")
+	public String openBoardDetail(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) {
+			// TODO => 올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+			return "redirect:/board/list.do";
+		}
+
+		BoardDTO board = boardService.getBoardDetail(idx);
+		if (board == null || "Y".equals(board.getDeleteYn())) {
+			// TODO => 없는 게시글이거나, 이미 삭제된 게시글이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+			return "redirect:/board/list.do";
+		}
+		model.addAttribute("board", board);
+
+		return "board/view";
+	}
+	
+	@PostMapping(value = "/board/delete.do")
+	public String deleteBoard(@RequestParam(value = "idx", required = false) Long idx) {
+		if (idx == null) {
+			// TODO => 올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+			return "redirect:/board/list.do";
+		}
+
+		try {
+			boolean isDeleted = boardService.deleteBoard(idx);
+			if (isDeleted == false) {
+				// TODO => 게시글 삭제에 실패하였다는 메시지를 전달
+			}
+		} catch (DataAccessException e) {
+			// TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+
+		} catch (Exception e) {
+			// TODO => 시스템에 문제가 발생하였다는 메시지를 전달
+		}
+
+		return "redirect:/board/list.do";
 	}
 }
